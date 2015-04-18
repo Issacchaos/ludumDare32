3d38844c13f1ca805b2208846bb2b8207e82d7b3
John Wilson <issacchaos@gmail.com>
2015-04-18 16:25:31 -0400

    healthbar

---
+++ ludumDare32/Assets/HealthBar.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+
+	public float healthWidth = 1;
+	public float currentHealth;
+	public float newWidth;
+	public Image healthBar;
+
+	// Use this for initialization
+	void Start () {
+		healthBar = gameObject.GetComponent<Image>();
+		currentHealth = gameObject.GetComponentInParent<CharacterBase> ().health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentHealth = gameObject.GetComponentInParent<CharacterBase> ().health;
+		newWidth = healthWidth * (currentHealth / 100); 
+		ResizeBar (healthBar, newWidth);
+
+	}
+
+	private void ResizeBar(Image bar, float percent)
+	{
+		if(bar)
+		{
+			bar.fillAmount = percent;
+		}
+	}
+}
+
+
+
