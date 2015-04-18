eaad92d164b44f51638f42e8da652886ec3d169c
coopdakilla523 <coopa523@gmail.com>
2015-04-17 23:15:49 -0400

    Added a player base script

---
+++ ludumDare32/Assets/Resources/Scripts/PlayerBase.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBase : CharacterBase 
+{
+
+	private Vector3 moveVec = Vector3.zero;
+
+	// Use this for initialization
+	void Start () 
+	{
+		moveSpeed = 25;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.W)) 
+		{
+			moveVec += transform.up;
+		}
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+			moveVec += (-transform.right);
+		}
+		if (Input.GetKeyDown (KeyCode.D)) 
+		{
+			moveVec += transform.right;
+		}
+		if (Input.GetKeyDown (KeyCode.S)) 
+		{
+			moveVec += (-transform.up);
+		}
+		moveVec.Normalize();
+		transform.position += (moveVec * moveSpeed * Time.deltaTime);
+	}
+}
